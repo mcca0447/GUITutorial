1a4514ed0d4c2ffff9a9bae4c1c06ecd4ab68aa5
James McCabe <mccabe.james24@gmail.com>
2018-05-27 23:30:16 -0400

    Added game loop code, game thread, start(), stop()

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,8 @@
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,13 +21,75 @@ public class Game extends Canvas implements Runnable{
         new Window(WIDTH, HEIGHT, "Let's Build a Game!", this);
     }
 
+    private Thread thread;
+    private boolean running = false;
+    
+    
    public synchronized void start(){
+       thread = new Thread(this);
+       thread.start();
+   }
+   
+   public synchronized void stop(){
+       try{
+           thread.join();
+       }catch(Exception e){
+           e.printStackTrace();
+       }
+   }
+   
+   public void run(){                                   //GAME LOOP
+       long lastTime = System.nanoTime();
+       double amountOfTicks= 60.0;
+       double ns = 1000000000 / amountOfTicks;
+       double delta = 0;
+       long timer = System.currentTimeMillis();
+       int frames = 0;
+       while(running){
+           long now = System.nanoTime();
+           delta += (now - lastTime)/ns;
+           lastTime = now;
+           while(delta >=1){
+               tick();
+               delta--;
+           }
+           if(running){
+               render();
+           }
+           frames++;
+           
+           if(System.currentTimeMillis()- timer > 1000){
+               timer += 1000;
+               System.out.println("FPS: " + frames);
+               frames = 0;
+           }
+       }
+       stop();
+   }
+   
+   private void tick(){
        
    }
    
-   public void run(){
+   private void render(){
+       BufferStrategy bs = this.getBufferStrategy();
+       if(bs == null){
+           this.createBufferStrategy(3);
+           return;
+       }
        
+       Graphics g = bs.getDrawGraphics();
+       
+       g.setColor(Color.green);
+       g.fillRect(0, 0, WIDTH, HEIGHT);
+       
+       g.dispose();
+       bs.show();
+       
+   
    }
+   
+   
    
    public static void main(String args[]){
        new Game();
@@ -32,4 +97,6 @@ public class Game extends Canvas implements Runnable{
    
     
     }
+
 }
+
